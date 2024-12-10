28240eec21605442876f8e54700a05c6fa8d2817
gaétan-lemont <gaetanl2004@gmail.com>
2024-12-10 11:10:13 +0100

    feat: add front authentifcation pages

--- front/app.py
+++ front/app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html", title="Page d'accueil")
+
+
+@app.route("/login")
+def login():
+    return render_template("auth/login.html", title="Page de connexion")
+
+
+@app.route("/register")
+def register():
+    return render_template("auth/register.html", title="Page d'inscription")
