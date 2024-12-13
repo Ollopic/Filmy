5a2e2cbdaba67aaf33a17b99cf99c6bbdb5d8740
gaétan-lemont <gaetanl2004@gmail.com>
2024-12-13 11:33:51 +0100

    feat: add front search

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -6,3 +6,8 @@ main_bp = Blueprint("main", __name__)
 @main_bp.route("/")
 def home():
     return render_template("main/index.html", title="Page d'accueil")
+
+
+@main_bp.route("/search")
+def search():
+    return render_template("main/search.html", title="Recherche")
