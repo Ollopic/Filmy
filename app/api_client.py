57540f33257825f63e0350b877d2e2d68bc3d9fe
gaétan-lemont <gaetanl2004@gmail.com>
2025-01-06 14:28:47 +0100

    feat: add login & registration & notification system

---
+++ app/api_client.py
@@ -0,0 +1,41 @@
+from logging import Logger, getLogger
+
+import requests
+
+
+class Client:
+    BASE_URL: str = "http://172.17.0.1:8001"
+
+    _logger: Logger = None
+
+    def __init__(self, logger: Logger = getLogger(__name__)):
+        self._logger = logger
+
+    def get_token(self, mail: str, password: str) -> str:
+        response = requests.post(
+            f"{self.BASE_URL}/token",
+            json={"mail": mail, "password": password},
+        )
+
+        response.raise_for_status()
+        dict_response = response.json()
+
+        return dict_response["token"]
+
+    def get_me(self, token: str):
+        response = requests.get(
+            f"{self.BASE_URL}/user/me",
+            headers={"Authorization": f"Bearer {token}"},
+        )
+        response.raise_for_status()
+
+        return response.json()
+
+    def create_user(self, username: str, mail: str, password: str):
+        response = requests.post(
+            f"{self.BASE_URL}/user",
+            json={"username": username, "mail": mail, "password": password},
+        )
+        response.raise_for_status()
+
+        return response.json()
--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
-from .blueprints import auth_bp, main_bp, movie_bp, artist_bp
+from . import config
+from .blueprints import artist_bp, auth_bp, main_bp, movie_bp
 
 app = Flask(__name__)
+app.secret_key = config.APP_SECRET_KEY
+
+
+@app.context_processor
+def is_user_connected():
+    token = request.cookies.get("token")
+    return dict(is_connected=token is not None)
+
 
 app.register_blueprint(main_bp)
 app.register_blueprint(auth_bp)
